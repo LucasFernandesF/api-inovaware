ea4f87150536ae80c6e6288c05961919fcb6173c
Lucas Fernandes <lucasffb120@gmail.com>
2025-06-03 19:58:07 -0300

    Teste simples modificando o README

--- app/calculo.py
+++ app/calculo.py
@@ -1,4 +1,4 @@
 def tempo_estimado(distancia_km: float, velocidade_kmh: float) -> float:
     if velocidade_kmh == 0:
         raise ValueError("Velocidade não pode ser zero")
-    return distancia_km / velocidade_kmh * 60  # retorna minutos
+    return distancia_km / velocidade_kmh * 60 
