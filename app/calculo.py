82d6c27cc1296f2b45b367957e83d9420039c4ae
Wallace Rodrigues <wallaceoliveira3001@gmail.com>
2025-06-16 19:27:15 -0300

    Adiciona documentação em linha para pull request

--- app/calculo.py
+++ app/calculo.py
@@ -1,4 +1,16 @@
 def tempo_estimado(distancia_km: float, velocidade_kmh: float) -> float:
-    if velocidade_kmh == 0:
-        raise ValueError("Velocidade não pode ser zero")
-    return distancia_km / velocidade_kmh * 60 
+    """Calcula o tempo estimado de viagem em minutos
+
+    Args:
+        distancia_km (float): _Distancia que será percorrida em quilometros.
+        velocidade_kmh (float): Velocidade em quilometros por hora
+
+    Raises:
+        ValueError: se a velocidade for menor ou igual a zero
+
+    Returns:
+        float: Retorna o tempo estimado em minutos
+    """
+    if velocidade_kmh <= 0:
+        raise ValueError("Velocidade não pode ser menor ou igual a zero")
+    return distancia_km / velocidade_kmh * 60
--- app/main.py
+++ app/main.py
@@ -3,9 +3,16 @@ from app.calculo import tempo_estimado
 
 app = FastAPI()
 
-@app.get("/tempo-estimado")
+@app.get("/tempo-estimado") 
 def calcular_tempo(distancia_km: float, velocidade_kmh: float):
+    """
+    Calcula o tempo estimado de uma viagem em minutos.
+
+    Recebe a distância e a velocidade como parâmetros e retorna o tempo total da viagem.
+    Levanta um erro 400 se a lógica de negócio falhar.
+    """
     try:
+        
         tempo = tempo_estimado(distancia_km, velocidade_kmh)
         return {"tempo_estimado_min": tempo}
     except ValueError as e:
